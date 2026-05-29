c0068f2a1d55d4522f3d17e5bf0fef0eb8c283d3
Breno Ferreira <breno.ferreira216@gmail.com>
2026-05-28 21:38:44 -0300

    feat: adiciona testes de PUT, DELETE e filtro por userId na API

--- src/test/java/api/UsuarioApiTest.java
+++ src/test/java/api/UsuarioApiTest.java
@@ -1,12 +1,12 @@
 package api;
 
 import io.restassured.RestAssured;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class UsuarioApiTest {
 
     @BeforeEach
@@ -14,7 +14,10 @@ public class UsuarioApiTest {
         RestAssured.baseURI = "https://jsonplaceholder.typicode.com";
     }
 
+    //GET
+
     @Test
+    @Order(1)
     public void deveRetornarListaDePosts() {
         given()
                 .when()
@@ -25,6 +28,7 @@ public class UsuarioApiTest {
     }
 
     @Test
+    @Order(2)
     public void deveRetornarPostPorId() {
         given()
                 .when()
@@ -32,10 +36,13 @@ public class UsuarioApiTest {
                 .then()
                 .statusCode(200)
                 .body("id", equalTo(1))
-                .body("title", not(emptyString()));
+                .body("userId", equalTo(1))
+                .body("title", not(emptyString()))
+                .body("body", not(emptyString()));
     }
 
     @Test
+    @Order(3)
     public void deveRetornar404ParaPostInexistente() {
         given()
                 .when()
@@ -44,7 +51,10 @@ public class UsuarioApiTest {
                 .statusCode(404);
     }
 
+    //POST
+
     @Test
+    @Order(4)
     public void deveCriarNovoPost() {
         String body = """
                 {
@@ -62,6 +72,59 @@ public class UsuarioApiTest {
                 .then()
                 .statusCode(201)
                 .body("title", equalTo("Teste"))
-                .body("id", not(emptyString()));
+                .body("body", equalTo("Criado via Rest Assured"))
+                .body("userId", equalTo(1))
+                .body("id", notNullValue());
+    }
+
+    //PUT
+
+    @Test
+    @Order(5)
+    public void deveAtualizarPostExistente(){
+        String body = """
+                {
+                "id": 1,
+                "title": "Título atualizado",
+                "body": "Conteúdo atualizado via Rest Assured",
+                "userId": 1
+                }
+                """;
+
+        given()
+                .contentType("application/json")
+                .body(body)
+                .when()
+                .put("/posts/1")
+                .then()
+                .statusCode(200)
+                .body("title", equalTo("Título atualizado"))
+                .body("body", equalTo("Conteúdo atualizado via Rest Assured"))
+                .body("id", equalTo(1));
+    }
+
+    //DELETE
+
+    @Test
+    @Order(6)
+    public void deveDeletarPostExistente(){
+        given()
+                .when()
+                .delete("/posts/1")
+                .then()
+                .statusCode(200);
+    }
+
+    @Test
+    @Order(7)
+    public void deveRetornarPostsPorUsuario(){
+        given()
+                .queryParam("userId", 1)
+                .when()
+                .get("/posts")
+                .then()
+                .statusCode(200)
+                .body("size()", equalTo(10))
+                .body("userId", everyItem(equalTo(1)));
     }
 }
